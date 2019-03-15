18a462ad360a0ac2bfa403464c05fc43f3802b61
ilarivp <45522896+ilarivp@users.noreply.github.com>
2019-03-16 01:26:43 +0200

    first working ui

--- modify.py
+++ modify.py
@@ -2,22 +2,16 @@ import numpy as np
 import os
 import textwrap
 import sys
+import juomarit as kulli
 
 def lisaa_pelaaja(juomarit, uusi_juomari):
-    for juomari in juomarit:
-        if juomari.name.lower() == uusi_juomari.lower():
-            break
-    new_juomari = juomari()
-    new_juomari.name = uusi_juomari
-    juomarit.append(new_juomari.lower())
+#    for nimi in juomarit:
+#        if nimi.name.lower() == uusi_juomari.lower():
+#            break
+    new_juomari = kulli.Juomari()
+    new_juomari.name = uusi_juomari.lower()
+    juomarit.append(new_juomari)
     return(juomarit)
-
-def lisaa_juoma(juomari, uusi_juoma):
-    for juoma in juomari.juomalista:
-        if juoma.lower() == uusi_juoma.lower():
-            break
-    juomari.juomalista.append(uusi_juoma.lower())
-    return(juomari)
 
 def poista_pelaaja(juomarit, poistettava_juomari):
     for juomari in juomarit:
@@ -26,12 +20,7 @@ def poista_pelaaja(juomarit, poistettava_juomari):
     juomarit.remove(poistettava_juomari)
     return(juomarit)
 
-def poista_juoma(juomari, poistettava_juoma):
-    for juoma in juomari.juomalista:
-        if juoma.lower() == poistettava_juoma.lower():
-            break
-    juomari.juomalista.remove(poistettava_juoma.lower())
-    return(juomari)
+
 
 def pelaa(juomari):
     numero = np.random.uniform()
